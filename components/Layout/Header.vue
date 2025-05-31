3a0e02604077477fbe70ebe6ae53b530797435ba
tangtang1b1b <a0981632653@gmail.com>
2025-05-31 21:50:03 +0800

    feat : 整理檔案更換icon

--- components/Layout/Header.vue
+++ components/Layout/Header.vue
@@ -22,26 +22,8 @@ onUnmounted(() => {})
 </script>
 
 <template>
-  <header :class="[isScroll ? 'shadow-nav' : '']" class="fixed left-0 top-0 z-100 h-[70px] w-full bg-txt-light">
-    <div class="z-50 mx-auto flex size-full justify-between max-w-screen-3xl items-center bg-txt-light px-5 duration-300">
-      <div class="flex h-full w-40 items-center justify-center">
-        <NuxtLinkLocale to="/" class="size-full content-center">
-          <img class="w-full" :src="fetchImg('/images/logo.png')" alt="logo" />
-        </NuxtLinkLocale>
-      </div>
-      <!-- <ul class="flex size-full justify-end dev-red">
-        <li
-          v-for="item in data"
-          :key="item.name"
-          class="flex h-full items-center justify-center text-white duration-300 hover:text-[#75fa90]"
-        >
-          <nuxt-link :to="item.url" class="h-full content-center px-5">{{ item.name }}</nuxt-link>
-        </li>
-      </ul> -->
-      <div class="flex divide-x text-white font-Montserrat">
-        <SwitchLocalePathLink locale="zh" :class="locale === 'zh' ? 'text-txt-super-light' : ''" class="px-2 hover:text-txt-super-light duration-300">中文</SwitchLocalePathLink>
-        <SwitchLocalePathLink locale="en" :class="locale === 'en' ? 'text-txt-super-light' : ''" class="px-2 hover:text-txt-super-light duration-300">English</SwitchLocalePathLink>
-      </div>
+  <header :class="[isScroll ? 'shadow-nav' : '']" class="fixed left-0 top-0 z-100 h-[70px] w-full bg-black">
+    <div class="z-50 mx-auto flex size-full justify-between max-w-screen-3xl items-center bg-black px-5 duration-300">
     </div>
   </header>
 </template>
